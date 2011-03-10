dee776cff6ec1468c79a339a6fa5eef8228debc0
Josh Allmann <joshua.allmann@gmail.com>
2011-03-09 15:04:25 -1000

    A little more neon work.

--- libswscale/arm/yuv2rgb_arm.c
+++ libswscale/arm/yuv2rgb_arm.c
@@ -2,14 +2,30 @@
 #include "libswscale/swscale_internal.h"
 
 #ifdef HAVE_NEON
-static void neon_yuv420_bgr24(uint8_t *Y, uint8_t *U, uint8_t *V, uint8_t *out,
-                            int w, uint32_t *coeffs)
+
+void ff_neon_yuv420_bgr24(const uint8_t *y, const uint8_t*u, const uint8_t*v,
+                          uint8_t*out, uint64_t *coeffs);
+static int neon_yuv420_bgr24(SwsContext *c, const uint8_t *src[], int srcStride[],
+                           int sliceY, int sliceH, uint8_t *dst[], int dstStride[])
 {
+    /*
+        R = Y' * Ycoef + Vred   * V'
+        G = Y' * Ycoef + Vgreen * V' + Ugreen * U'
+        B = Y' * Ycoef               + Ublue  * U'
+
+        Where
+        X' = X * 8 - Xoffset
+     */
+    ff_neon_yuv420_bgr24(src[0], src[1], src[2], dst[0], &c->yCoeff);
+
+    av_log(NULL, AV_LOG_ERROR, "yCoeff: %llu, recevied %llu\n", c->yCoeff, (uint64_t)dst[0]);
+    return sliceH;
 }
 
-static void neon_yuv420_rgb24(uint8_t *Y, uint8_t *U, uint8_t *V, uint8_t *out,
-                            int w, uint32_t *coeffs)
+static int neon_yuv420_rgb24(SwsContext *c, const uint8_t *sr[], int srcStride[],
+                          int sliceY, int sliceH, uint8_t *dst[], int dstStride[])
 {
+    return sliceH;
 }
 
 SwsFunc ff_yuv2rgb_init_neon(SwsContext *c)
@@ -20,7 +36,7 @@ SwsFunc ff_yuv2rgb_init_neon(SwsContext *c)
            sws_format_name(c->srcFormat), sws_format_name(c->dstFormat));
 
     switch (c->dstFormat) {
-    case PIX_FMT_RGB24: return neon_yuv420_rgb24;
+    case PIX_FMT_RGB24:
     case PIX_FMT_BGR24: return neon_yuv420_bgr24;
     default: return NULL;
     }
